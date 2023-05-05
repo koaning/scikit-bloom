2e8408d5e15686d849754b4248ff4e1a865f921c
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-05-05 13:49:47 +0000

    add BloomishVectorizer

--- skbloom/__init__.py
+++ skbloom/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.sparse import csr_matrix, dok_array
 from sklearn.base import BaseEstimator, TransformerMixin
+from skpartial.pipeline import make_partial_union
+from sklearn.feature_extraction.text import HashingVectorizer
 
 
 class BloomVectorizer(BaseEstimator, TransformerMixin):
@@ -11,6 +13,9 @@ class BloomVectorizer(BaseEstimator, TransformerMixin):
     
     def fit(self, X, y=None):
         return self 
+    
+    def partial_fit(self, X, y=None, classes=None):
+        return self 
 
     def transform(self, X, y=None):
         row, col = [], []
@@ -20,3 +25,23 @@ class BloomVectorizer(BaseEstimator, TransformerMixin):
                     row.append(i)
                     col.append(hash(f"{_}-{w}") % self.n_buckets)
         return csr_matrix((np.ones(len(row)), (row, col)), dtype=np.int8)
+
+
+
+class BloomishVectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, n_buckets=2000, n_hash=3, lowercase=True, ngram_range=(1, 1), analyzer="word") -> None:
+        self.n_buckets = n_buckets
+        self.n_hash = n_hash
+        self.lowercase = lowercase
+        self.pipe = make_partial_union(*[
+            HashingVectorizer(n_features=n_buckets + i, ngram_range=ngram_range, analyzer=analyzer) for i in range(n_hash)
+        ])
+    
+    def fit(self, X, y=None):
+        return self 
+    
+    def partial_fit(self, X, y=None, classes=None):
+        return self 
+
+    def transform(self, X, y=None):
+        return self.pipe.transform(X, y)
